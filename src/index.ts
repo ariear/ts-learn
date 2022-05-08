993dbc512b0b51a2d582d09d7b528a512c64d7ef
arie <arieinjector@gmail.com>
2022-05-08 14:57:01 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+const sapa : string = 'Hello World'
+const nomor : number = 10
+const rill : boolean = true
+const food : object = [
+    {
+        nama: 'Rujak Mie'
+    },
+    {
+        nama: 'Jangan Bening'
+    }
+]
+let drink : object = {nama: 'Coca Cola'}
+
+const jml = (a: number, b : number) : number => {
+    return a + b
+}
+
+// this is union variable
+let multi : string | number = "Aku string njir"
+multi = 69
+
+type Satir = string
+const bilek : Satir = 'Bang gweh satir'
+
